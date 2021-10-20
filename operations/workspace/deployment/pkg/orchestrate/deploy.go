b2403fb8a0d1815c6856bace3f74c06c2ad53a5e
Prince Rachit Sinha <prince@gitpod.io>
2021-10-20 07:54:25 +0000

    Add orchestration code

--- operations/workspace/deployment/pkg/orchestrate/deploy.go
+++ operations/workspace/deployment/pkg/orchestrate/deploy.go
@@ -1,26 +1,49 @@
 package orchestrate
 
 import (
+	"sync"
+
 	"github.com/gitpod-io/gitpod/ws-deployment/pkg/check/prerun"
 	"github.com/gitpod-io/gitpod/ws-deployment/pkg/common"
 	v1 "github.com/gitpod-io/gitpod/ws-deployment/pkg/config/v1"
 	"github.com/gitpod-io/gitpod/ws-deployment/pkg/step"
 )
 
-func Deploy(c *v1.Config, environment *common.Environment) {
-	createClustersStep := buildCreateClustersStep(c, environment)
-	// buildInstallGitpodStep()
-	// other steps
-	// decide how to run those steps, in parallel?
+func Deploy(c *v1.Config, environment *common.Environment) error {
+	createClustersSteps, err := buildCreateClustersStep(c, environment)
+	if err != nil {
+		return err
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(len(createClustersSteps))
+	defer wg.Wait()
+
+	for i := range createClustersSteps {
+		stepToExecute := createClustersSteps[i]
+		go func() {
+			defer wg.Done()
+			stepToExecute.Run()
+		}()
+	}
+	// TODO(prs): This will change once we have code handling error cases
+	// for building cluster
+	return nil
 }
 
-func buildCreateClustersStep(config *v1.Config, environment *common.Environment) []step.Step {
+func buildCreateClustersStep(config *v1.Config, environment *common.Environment) ([]step.Step, error) {
 	projectContext := &common.ProjectContext{
 		Environment: *environment,
 	}
+	steps := make([]step.Step, 0, 0)
 	for _, wc := range config.WorkspaceClusters {
-		buildCreateClusterStep(projectContext, &wc)
+		step, err := buildCreateClusterStep(projectContext, &wc)
+		if err != nil {
+			return nil, err
+		}
+		steps = append(steps, step)
 	}
+	return steps, nil
 }
 
 func buildCreateClusterStep(projectContext *common.ProjectContext, workspaceCluster *common.WorkspaceCluster) (step.Step, error) {
